f63b6c3166dc7419d553f9cd4257684219762f01
AngelVOC <angelmystic15@gmail.com>
2026-05-07 16:27:29 +1200

    Feat: added vercel analytic tracking, to track website visitor

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ import Skills     from './components/Skills';
 import Projects   from './components/Projects';
 import Experience from './components/Experience';
 import Footer     from './components/Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 
 export default function App() {
   return (
     <>
+        <Analytics />
         <Cursor />
         <Nav />
         <Hero />
